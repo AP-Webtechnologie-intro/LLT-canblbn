c8feb0b49c94ece0ece9a99aa3a3258d6b8ab3e6
canblbn <147602989+canblbn@users.noreply.github.com>
2023-12-26 22:24:33 +0100

    First commit

---
+++ map.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([40.7577, -74.0028], 15);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
